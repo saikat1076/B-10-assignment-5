ec2187aead3c9a2c0802251875604d61d61ef36f
saikat1076 <chakrabortymoni335@gmail.com>
2024-09-25 03:50:48 +0600

    add blog

--- js/index.js
+++ js/index.js
@@ -79,5 +79,10 @@ function getTextFieldValueById(id) {
         document.getElementById('show-donation').classList.remove('bg-[#B4F461]');
         document.getElementById('show-history').classList.remove('bg-[#B4F461]', 'bg-inherit');
         document.getElementById(id).classList.add('bg-[#B4F461]');
-    }        
-            
+    }   
+document.getElementById('blog-button').addEventListener('click', function(){
+    window.location.href = '/blog.html';
+})            
+document.getElementById('home-button').addEventListener('click', function(){
+    window.location.href = '/index.html';
+})            
